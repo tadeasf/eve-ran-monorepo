21126aa27246fc3ea44c31304149f77ae83dbbe6
Tadeáš Fořt <business@tadeasfort.com>
2024-10-03 13:48:17 +0200

    refactor(region.go): add Scan method to Region model for unmarshalling JSONB values to improve code readability and maintainability

--- backend/src/db/models/region.go
+++ backend/src/db/models/region.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 )
 
 // Region model
@@ -10,4 +12,29 @@ type Region struct {
 	Name           string          `json:"name"`
 	Description    string          `json:"description"`
 	Constellations json.RawMessage `gorm:"type:jsonb" json:"constellations"`
+}
+
+func (r *Region) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
+	}
+
+	result := &struct {
+		RegionID       int             `json:"region_id"`
+		Name           string          `json:"name"`
+		Description    string          `json:"description"`
+		Constellations json.RawMessage `json:"constellations"`
+	}{}
+
+	if err := json.Unmarshal(bytes, &result); err != nil {
+		return err
+	}
+
+	r.RegionID = result.RegionID
+	r.Name = result.Name
+	r.Description = result.Description
+	r.Constellations = result.Constellations
+
+	return nil
 }
