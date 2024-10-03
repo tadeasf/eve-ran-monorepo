429fdc91f5f9d48f2416898bdf78a88c70499d39
Tadeáš Fořt <business@tadeasfort.com>
2024-10-03 12:59:22 +0200

    refactor(system.go): change type of Planets field to json.RawMessage for flexibility in handling JSON data

--- backend/src/db/models/system.go
+++ backend/src/db/models/system.go
@@ -1,15 +1,17 @@
 package models
 
+import "encoding/json"
+
 // System model
 type System struct {
-	SystemID        int       `gorm:"primaryKey" json:"system_id"`
-	ConstellationID int       `json:"constellation_id"`
-	Name            string    `json:"name"`
-	SecurityClass   string    `json:"security_class"`
-	SecurityStatus  float64   `json:"security_status"`
-	StarID          int       `json:"star_id"`
-	Planets         []int     `gorm:"type:jsonb" json:"planets"`
-	Stargates       []int     `gorm:"type:jsonb" json:"stargates"`
-	Stations        []int     `gorm:"type:jsonb" json:"stations"`
-	Position        *Position `gorm:"type:jsonb" json:"position"`
+	SystemID        int             `gorm:"primaryKey" json:"system_id"`
+	ConstellationID int             `json:"constellation_id"`
+	Name            string          `json:"name"`
+	SecurityClass   string          `json:"security_class"`
+	SecurityStatus  float64         `json:"security_status"`
+	StarID          int             `json:"star_id"`
+	Planets         json.RawMessage `gorm:"type:jsonb" json:"planets"`
+	Stargates       []int           `gorm:"type:jsonb" json:"stargates"`
+	Stations        []int           `gorm:"type:jsonb" json:"stations"`
+	Position        *Position       `gorm:"type:jsonb" json:"position"`
 }
