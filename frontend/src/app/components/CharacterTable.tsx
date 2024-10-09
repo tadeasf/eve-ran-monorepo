94aa09cd2dfaa50f9cdc3adf80b7d09f23f0cd72
tadeasf <business@tadeasfort.com>
2024-10-09 09:36:55 +0200

    feat: Update Dockerfile to use latest official Bun image for better consistency and performance

--- frontend/src/app/components/CharacterTable.tsx
+++ frontend/src/app/components/CharacterTable.tsx
@@ -1,3 +1,20 @@
+// Copyright (C) 2024 Tadeáš Fořt
+// 
+// This file is part of EVE Ran Services.
+// 
+// EVE Ran Services is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// EVE Ran Services is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with EVE Ran Services.  If not, see <https://www.gnu.org/licenses/>.
+
 import { useState } from 'react'
 import { CharacterStats, Kill } from '../../lib/types'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/app/components/ui/table"
